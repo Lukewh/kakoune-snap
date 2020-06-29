fa3c1255b687ba563fffef10de5ec10cd280b307
Lukewh <luke@lukewh.com>
2020-06-29 11:53:20 +0100

    Update to "v2020.01.16"

---
+++ updater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+json=$(curl --silent "https://api.github.com/repos/mawww/kakoune/releases/latest")
+
+current_version=""
+
+if [ -f "current_version" ]; then
+    current_version=$(cat "$current_version")
+fi
+
+latest_version=$(echo "$json" | jq 'if .prerelease == false then .tag_name else null end')
+
+if [ "$current_version" != "$latest_version" ]; then
+    echo "Update to $latest_version"
+    sed 's/\$version/'${latest_version}'/g' _snapcraft.yaml > snapcraft.yaml
+    echo "Updated"
+
+    git commit -a -m "Update to $latest_version"
+    git push origin master
+
+    echo "$current_version" > current_version
+fi
+
