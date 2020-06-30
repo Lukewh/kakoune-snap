b41a40eedbdf36d500adbc6f84012e22c09e7bea
Lukewh <luke@lukewh.com>
2020-06-30 11:46:19 +0100

    Update updater script

--- updater.sh
+++ updater.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 date
+pwd
 json=$(curl --silent "https://api.github.com/repos/mawww/kakoune/releases/latest")
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "$BASE_DIR"
 
 current_version=""
 
 if [ -f "current_version" ]; then
-    current_version=$(cat current_version)
+    current_version=$(cat "$BASE_DIR/current_version")
 fi
 
 latest_version=$(echo "$json" | jq -r 'if .prerelease == false then .tag_name else null end')
@@ -20,6 +25,6 @@ if [ "$current_version" != "$latest_version" ]; then
     git commit -a -m "Update to $latest_version"
     git push origin master
 
-    echo "$latest_version"> current_version
+    echo "$latest_version"> "$BASE_DIR/current_version"
 fi
 
