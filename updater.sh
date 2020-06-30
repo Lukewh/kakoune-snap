513e1acad730d85b07bde07516b5bab29543cd0d
Lukewh <luke@lukewh.com>
2020-06-30 11:55:49 +0100

    Update updater

--- updater.sh
+++ updater.sh
@@ -5,12 +5,12 @@ json=$(curl --silent "https://api.github.com/repos/mawww/kakoune/releases/latest
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "$BASE_DIR"
+cd "$BASE_DIR"
 
 current_version=""
 
 if [ -f "current_version" ]; then
-    current_version=$(cat "$BASE_DIR/current_version")
+    current_version=$(cat "current_version")
 fi
 
 latest_version=$(echo "$json" | jq -r 'if .prerelease == false then .tag_name else null end')
@@ -19,12 +19,12 @@ echo "$latest_version"
 
 if [ "$current_version" != "$latest_version" ]; then
     echo "Update to $latest_version"
-    sed 's/\$version/'${latest_version}'/g' "$BASE_DIR/_snapcraft.yaml" > "$BASE_DIR/snapcraft.yaml"
+    sed 's/\$version/'${latest_version}'/g' "_snapcraft.yaml" > "snapcraft.yaml"
     echo "Updated"
 
     git commit -a -m "Update to $latest_version"
     git push origin master
 
-    echo "$latest_version"> "$BASE_DIR/current_version"
+    echo "$latest_version"> "current_version"
 fi
 
